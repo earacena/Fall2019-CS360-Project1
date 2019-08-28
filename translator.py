4f26d93cc3d009e723d5aa2ae9a278dc9d42aa37
earacena <earacenadev@gmail.com>
2019-08-28 13:33:15 -0400

    Initial commit, added work completed so far:
    translator.py can read source code from file, remove all excess noise such as
    whitespace and newlines, then parses this into a lists of keywords
    corresponding to each individual function.

---
+++ translator.py
@@ -0,0 +1,72 @@
+# Project:      Translate source code to assembly (C++ to x86_64)
+# Author:       Emanuel Aracena
+# Date created: August 28, 2019
+# Name of file: translator.py (emanuel_aracena_translator.py)
+# Description: Translator removes all white space and excess characters like
+#              newlines, then parses based on syntax
+
+def read_source_code(filename):
+    source_code = []
+    with open("example.cpp") as file:
+        for line in file:
+            source_code = source_code + line.split(" ")
+    return source_code
+
+def clean_up(source_code):
+    # Turns all newline chars into whitespace or erases them from string
+    source_code = [elem.strip('\n') for elem in source_code]
+
+    # Use a filter to pick out all elements that are not whitespace (' ')
+    # The lambda takes an element from source_code, if it is not '', it doesn't
+    # get filtered
+    source_code = list(filter(lambda elem: elem != '', source_code))    
+    return source_code
+
+def parse_source_into_functions(source_code):
+    index1 = 0
+    index2 = 0
+    function_split = []
+    flag = False
+    while flag != True:
+        # Move two markers from start of one function to end, using the '}' as
+        # an indicator of the end of a function
+        index1 = index2
+        index2 = source_code.index("}", index1)
+        function_split = function_split + [source_code[index1:index2] + ['}']]
+        
+        # Allow index to find following right curly brace
+        try:
+            source_code.remove("}")
+        except ValueError:
+            pass
+
+        # Debug
+        # print(index1, index2)
+
+        # If all curly braces have been removed and index2 reached the end,
+        # set flag true
+        if index2 == len(source_code):
+            flag = True
+
+    # Remove return types
+    for function in function_split:
+        del function[0]
+        
+    return function_split
+
+# def parse_functions_into_asm(parsed_func_list):
+#     for function in parsed_func_list:
+#         for keyword in function:
+            
+
+
+def main():
+    filename = "example.cpp"
+    source = read_source_code(filename)
+    source = clean_up(source)
+    parsed_func_list = parse_source_into_functions(source)
+    print(parsed_func_list)
+    
+    
+if __name__ == "__main__":
+    main()
